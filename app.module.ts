7a36f122efe3593d97fd2de3c4dfd79213f84a48
rujita <lagerujita127@gmail.com>
2024-09-05 22:21:37 +0545

    first commit

---
+++ app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, TodolistComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
